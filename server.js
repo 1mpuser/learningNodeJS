76db46ecd69ebb3ed5469a1f10ebbb6eb6d348cb
Алексей Баранов <alex022003@mail.ru>
2022-06-15 22:21:59 +0300

    First express server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import _ from 'underscore';
-import __dirname from './DirnamePath/__dirname.js';
+import __dirname from './__dirname.js';
 import http from 'http';
 import { constants } from 'fs';
+import express, { response } from 'express';
+import { request } from 'https';
 // const text = fs.readFileSync('file.txt', 'utf-8');
 // fs.writeFileSync('OneMoreFile.txt', text);
 // const obj = {
@@ -30,11 +32,47 @@ import { constants } from 'fs';
 // 	writebleStream.write(`${i}\n`);
 // }
 // writebleStream.end();
-http
-	.createServer((request, responce) => {
-		responce.setHeader('Cache-Control', 'no-cache');
-		responce.statusCode = 200;
-		responce.write('Da mihi esse optimum');
-		responce.end();
-	})
-	.listen(4000);
+// http
+// 	.createServer((request, response) => {
+// 		console.log(request.url);
+// 		console.log(request.method);
+// 		console.log(request.headers);
+
+// 		response.writeHead(200, { 'Content-Type': 'text/html' });
+// 		response.write('text');
+// 		response.end();
+// 	})
+// 	.listen(4000);
+const app = express();
+// app.get('/', (req, res) => {
+// 	res.send('Da mihi esse optimum');
+// });
+app.get('/kek', (req, resp) => {
+	resp.send('Krek ne kek');
+	console.log(req.originalUrl);
+});
+// app.get('/todos', (req, res) => {
+// 	res.send(req.query);
+// 	console.log(req.query);
+// });
+app.get('/protocol', (req, res) => res.send(req.protocol));
+app.get('/secure', (req, res) => res.send(req.secure));
+app.get('/headers', (req, res) => res.send(req.headers));
+app.get('/languages', (req, res) => res.send(req.acceptsLanguages));
+app.get('/ipshka', (req, res) => res.send(req.ip));
+app.get('/page/adasd/', (req, res) => res.redirect(301, '/'));
+// app.get('/', (req, res) => {
+// 	res.type('json');
+// 	const obj = {
+// 		name: 'Alex',
+// 		password: 3228,
+// 		email: 'khuilo@mail.com',
+// 		date: new Date(),
+// 	};
+// 	const josn = JSON.stringify(obj);
+// 	res.json(obj);
+// });
+app.get('/profiles', (request, response) => {
+	response.sendFile(__dirname + '/randomJson.json');
+});
+app.listen(4000, () => console.log('Running...'));
